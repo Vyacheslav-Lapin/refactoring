82a630f2324a4e483c52608fe2599df484cf91a5
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2017-12-26 11:27:36 +0500

    String interpolation (templating)

--- src/main/java/ru/vlapin/examples/dbcp/ConnectToDB.java
+++ src/main/java/ru/vlapin/examples/dbcp/ConnectToDB.java
@@ -3,7 +3,10 @@ package ru.vlapin.examples.dbcp;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @Log4j2
 public class ConnectToDB {
@@ -23,9 +26,11 @@ public class ConnectToDB {
                     "INSERT INTO students (name, id_group) VALUES ('Баба-Яга', 123456)");
 
             try (ResultSet rs = st.executeQuery("SELECT * FROM students")) {
-                while (rs.next()) {
-                    System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3));
-                }
+                while (rs.next())
+                    System.out.printf("%d %s %d%n",
+                            rs.getInt(1),
+                            rs.getString(2),
+                            rs.getInt(3));
             }
         }
     }
